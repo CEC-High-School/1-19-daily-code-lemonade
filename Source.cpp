713409d8c654b69bd45099d034275fdee359e38a
HarleyQuinn34 <35504117+HarleyQuinn34@users.noreply.github.com>
2018-01-19 12:56:43 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+for (int i = 256; i> 28; i=i-4) 
+	cout << i << endl;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+}
